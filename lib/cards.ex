65023a741be594caa6f6c97dfb68b2766890c7b9
Jason Hanna <jason.hanna01@gmail.com>
2016-12-13 14:07:39 -0700

    add load method

--- lib/cards.ex
+++ lib/cards.ex
@@ -26,4 +26,11 @@ defmodule Cards do
     File.write(filename, binary)
   end
 
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "This file does not exist"
+    end
+  end
+
 end
